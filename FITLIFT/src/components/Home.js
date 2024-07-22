32f366977e0054ad5bbd1df527a84496b4cfa899
vishuchaudhary4746 <vishuchaudhary4746@gmail.com>
2024-07-22 16:47:32 +0530

    hbbhb

--- FITLIFT/src/components/Home.js
+++ FITLIFT/src/components/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container, Typography, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-
 const Home = () => {
     return (
         <Container style={{ textAlign: 'center', padding: '2rem' }}>
--- README.md
+++
@@ -1,2 +0,0 @@
-# FitLift
- Fitness app
